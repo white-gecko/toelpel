a3c998a98d2828edb973e19801bc09ccd2a97aaf
Natanael Arndt <arndtn@gmail.com>
2025-02-07 21:35:46 +0100

    init file for git tests

---
+++ tests/test_git.py
@@ -0,0 +1,18 @@
+from git_watchtower.git import git
+from pathlib import Path
+import os
+from shutil import copyfile
+
+test_directory = Path(os.path.dirname(__file__))
+
+
+def test_is_repo(tmp_path):
+    return
+
+
+def test_remotes(tmp_path):
+    return
+
+
+def test_branches(tmp_path):
+    return
